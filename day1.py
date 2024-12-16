8039288f479717bead90253f2190e389a1b21d00
John Dott <jkd28@pitt.edu>
2024-12-16 00:36:11 -0500

    Find total difference
    I also had to ensure I was converting the str to int on read-in, but
    that is trivial

--- day1.py
+++ day1.py
@@ -2,8 +2,8 @@ def parse_text_file():
   with open("day1input.txt", "r") as inputfile:
     line_nums = [line.strip().split('   ') for line in inputfile]
     # print(line_nums)
-    list1 = [pairs[0] for pairs in line_nums]
-    list2 = [pairs[1] for pairs in line_nums]
+    list1 = [int(pairs[0]) for pairs in line_nums]
+    list2 = [int(pairs[1]) for pairs in line_nums]
   return [list1, list2]
 
 def day1_runner():
@@ -13,12 +13,17 @@ def day1_runner():
   # sort both lists
   sorted1 = sorted(lists[0])
   sorted2 = sorted(lists[1])
-  print(sorted1)
 
   # calc differences and sum
-  if len(sorted1) =! len(sorted2):
+  if len(sorted1) != len(sorted2):
     print("PROBLEM! LIST LENGTHS DON'T MATCH")
 
+  total_dif = 0
+  for num1, num2 in zip(sorted1, sorted2):
+    print(num1, num2)
+    total_dif = total_dif + abs(num1-num2)
+
+  print(total_dif)
 
 if __name__ == "__main__":
   day1_runner()
