7a4f8d213ab4d9a4d0c90ad117970812e9d5b59e
John Dott <jkd28@pitt.edu>
2024-12-16 12:36:20 -0500

    Adjust regex and add state for D3P2
    Maintain state of enabled vs. disabled as specified in instrucitons. A
    small modification to the regex and accompanying instruction matching is
    all that's needed to accomplish this.

--- day3.py
+++ day3.py
@@ -1,19 +1,30 @@
 import re
 
 def parse_instructions():
-  regex = r"mul\(\d{1,3},\d{1,3}\)"
+  regex = r"(don?'?t?\(\))|(mul\(\d{1,3},\d{1,3}\))"
   corrupted_mem = ""
   with open("day3input.txt", "r", encoding="UTF-8") as infile:
     corrupted_mem = infile.read()
 
+  # using capture groups creates a tuple, so 'do/don't' instructions are in group 0 
+  # and 'mul' instructions are in group 1
   instructions = re.findall(regex, corrupted_mem)
   return instructions
 
 def perform_instructions(instructions):
   sum = 0
+  enabled = True
   for instruction in instructions:
-    nums = re.findall(r'\d{1,3}', instruction)
-    sum += int(nums[0]) * int(nums[1])
+    if instruction[0]:
+      if instruction[0] == "don't()":
+        enabled = False
+      if instruction[0] == "do()":
+        enabled = True
+        
+    elif instruction[1]:
+      if enabled:
+        nums = re.findall(r'\d{1,3}', instruction[1])
+        sum += int(nums[0]) * int(nums[1])
   return sum
 
 if __name__ == "__main__":
