dc53ae704099c8bd9f60adf61be9db8965f64f22
John Dott <jkd28@pitt.edu>
2024-12-16 13:12:50 -0500

    D4P1: Solve via hard-coded directional checks
    This is definitely not the most efficient way to handle this, but seeing
    as n=4 here, I think I'll be ok

--- day4.py
+++ day4.py
@@ -5,6 +5,43 @@ def process_input():
       crossword.append(list(line))
   return crossword
 
+def find_xmas(crossword):
+  occurrences = 0
+  for y, row in enumerate(crossword):
+    for x, col in enumerate(row):
+      if col == "X":
+        #try left
+        if x >= 3 and crossword[y][x-1] == "M" and crossword[y][x-2] == "A" and crossword[y][x-3] == "S":
+          occurrences += 1
+        #try right
+        if x <= len(row)-4 and crossword[y][x+1] == "M" and crossword[y][x+2] == "A" and crossword[y][x+3] == "S":
+          occurrences += 1
+        #try up
+        if y >=3 and crossword[y-1][x] == "M" and crossword[y-2][x] == "A" and crossword[y-3][x] == "S":
+          occurrences += 1
+        #try down
+        if y <= len(crossword)-4 and crossword[y+1][x] == "M" and crossword[y+2][x] == "A" and crossword[y+3][x] == "S":
+          occurrences += 1
+        
+        # left-down diagonal
+        if x >= 3 and y <= len(crossword)-4 and crossword[y+1][x-1] == "M" and crossword[y+2][x-2] == "A" and crossword[y+3][x-3] == "S":
+          occurrences += 1
+        # left-up diagonal
+        if x >= 3 and y >=3 and crossword[y-1][x-1] == "M" and crossword[y-2][x-2] == "A" and crossword[y-3][x-3] == "S":
+          occurrences += 1
+        # right-down diagonal
+        if x <= len(row)-4 and y <= len(crossword)-4 and crossword[y+1][x+1] == "M" and crossword[y+2][x+2] == "A" and crossword[y+3][x+3] == "S":
+          occurrences += 1
+        # right-up diagonal
+        if x <= len(row)-4 and y >=3 and crossword[y-1][x+1] == "M" and crossword[y-2][x+2] == "A" and crossword[y-3][x+3] == "S":
+          occurrences += 1
+      else:
+        continue
+  return occurrences
+
+      
+
 if __name__ == "__main__":
   crossword = process_input()
-  print(crossword)
+  xmas_occurrances = find_xmas(crossword)
+  print(f"XMAS occurances: {xmas_occurrances}")
